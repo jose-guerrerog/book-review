fbd302511bc3389f9b0fcff60b4941e1889c8a91
jose-guerrerog <joseguerrerog880@gmail.com>
2025-03-03 05:38:22 +1100

    trying to fix errors

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,7 @@ class BooksController < ApplicationController
   before_action :find_book, only: [:show, :edit, :update, :destroy]
   def index
     if params[:category].blank?
-      @books = Book.all.order("created_at DESC")
+      @books = Book.all.order('created_at DESC')
     else
       @category_id = Category.find_by(name: params[:category]).id
       @books = Book.where(:category_id => @category_id).order('created_at DESC')
