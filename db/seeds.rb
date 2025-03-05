c0fb1588aa3a7b99e72c826aa46867a3638cb4f9
jose-guerrerog <joseguerrerog880@gmail.com>
2025-03-06 07:39:36 +1100

    fix seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -44,7 +44,7 @@ books_data = [
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
     category_id: Category.find_by(name: "Fantasy").id,
     cover_image: "narnia.jpg",
-    user_id: 1
+    user_id: 1,
   },
   {
     title: "Harry Potter and the Philosopher's Stone",
@@ -52,7 +52,7 @@ books_data = [
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
     category_id: Category.find_by(name: "Fantasy").id,
     cover_image: "hp1.jpg",
-    user_id: 1
+    user_id: 1,
   },
   {
     title: "Aerial Robotics: With STM32F100RB Microcontroller",
@@ -60,7 +60,7 @@ books_data = [
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
     category_id: Category.find_by(name: "Technology").id,
     cover_image: "aerial.jpg",
-    user_id: 1
+    user_id: 1,
   },
   {
     title: "Elon Musk: A Biography of an Entrepreneur and Innovator",
@@ -68,30 +68,30 @@ books_data = [
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
     category_id: Category.find_by(name: "Biography").id,
     cover_image: "elon_musk.jpg"
-    user_id: 1
+    user_id: 1,
   },
 ]
 
-# Create books and attach cover images
-books_data.each do |book_data|
-  book = Book.find_or_create_by!(title: book_data[:title]) do |b|
-    b.author = book_data[:author]
-    b.description = book_data[:description]
-    b.category_id = book_data[:category_id]
-    b.user_id = user.id
-  end
+# # Create books and attach cover images
+# books_data.each do |book_data|
+#   book = Book.find_or_create_by!(title: book_data[:title]) do |b|
+#     b.author = book_data[:author]
+#     b.description = book_data[:description]
+#     b.category_id = book_data[:category_id]
+#     b.user_id = user.id
+#   end
 
-  # Only attach image if the book doesn't already have one
-  unless book.avatar.attached?
-    begin
-      book.avatar.attach(
-        io: open_file(book_data[:cover_image]),
-        filename: book_data[:cover_image],
-        content_type: "image/jpeg"
-      )
-      puts "Attached cover image to: #{book.title}"
-    rescue Errno::ENOENT => e
-      puts "Warning: Could not find image file #{book_data[:cover_image]} for book '#{book.title}'. Error: #{e.message}"
-    end
-  end
-end
+#   # Only attach image if the book doesn't already have one
+#   unless book.avatar.attached?
+#     begin
+#       book.avatar.attach(
+#         io: open_file(book_data[:cover_image]),
+#         filename: book_data[:cover_image],
+#         content_type: "image/jpeg"
+#       )
+#       puts "Attached cover image to: #{book.title}"
+#     rescue Errno::ENOENT => e
+#       puts "Warning: Could not find image file #{book_data[:cover_image]} for book '#{book.title}'. Error: #{e.message}"
+#     end
+#   end
+# end
