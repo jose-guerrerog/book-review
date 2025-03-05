f50da727396ed87474822596785bf1ab10e5a56c
jose-guerrerog <joseguerrerog880@gmail.com>
2025-03-05 13:26:37 +1100

    change locations css

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,6 +2,8 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = "1.0"
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets')
+
 # Rails.application.config.assets.precompile += %w(application.js)
 
 # Add additional assets to the asset load path.
