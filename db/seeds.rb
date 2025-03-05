37ffe97fa135cfd6b87018b8fb1143ff2e897a74
jose-guerrerog <joseguerrerog880@gmail.com>
2025-03-06 07:57:37 +1100

    added comma

--- db/seeds.rb
+++ db/seeds.rb
@@ -67,7 +67,7 @@ books_data = [
     author: "Ross Danvers",
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
     category_id: Category.find_by(name: "Biography").id,
-    cover_image: "elon_musk.jpg"
+    cover_image: "elon_musk.jpg",
     user_id: 1,
   },
 ]
